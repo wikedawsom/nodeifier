1ef23b7228e38a10ff25df993e359b5f658f41c4
wike <cp_harris1011@hotmail.com>
2021-06-23 23:53:53 -0400

    begin implementing auto-buy functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const axios = require('axios');
 const winston = require('winston');
+const { JSDOM } = require('jsdom');
 
 // Init error logger
 const logger = winston.createLogger({
@@ -13,7 +14,13 @@ const logger = winston.createLogger({
 	],
 });
 
-const { PING_GROUP_ID, WEBHOOK_ID, WEBHOOK_TOKEN, WEBHOOK_FULL_URL } = process.env;
+const { 
+	PING_GROUP_ID, 
+	WEBHOOK_ID, 
+	WEBHOOK_TOKEN, 
+	WEBHOOK_FULL_URL, 
+	AUTO_BUY, 
+	PRODUCT_URL } = process.env;
 let hook;
 // If the webhook url is goven, use that
 // otherwise see if Token and Id are given
@@ -44,7 +51,9 @@ const startTimestamp = new Date();
 logger.error("Script started, log initialized :) " + startTimestamp.toDateString());
 
 hook.send(`Beware ${PING_TAG}! I live`);
+hook.send(`Auto purchase ${AUTO_BUY ? "*enabled*" : "*disabled*"}`)
 let iterator = 0;
+let purchased = false;
 const listenLoop = () => {
 	if(iterator % 1000 === 0 && iterator !== 0 || iterator === 5){
 		const currentTimestamp = new Date();
@@ -55,7 +64,7 @@ const listenLoop = () => {
 		minutes = minutes - hours * 60;
 		hook.send(`Uptime: ${hours < 10 ? "0" : ""}${hours}:${minutes < 10 ? "0" : ""}${minutes}:${seconds < 10 ? "0" : ""}${seconds}. Currently no end in sight`);
 	}
-	axios.get('https://www.bestbuy.com/site/nvidia-geforce-rtx-3090-24gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429434.p?skuId=6429434')
+	axios.get(PRODUCT_URL)
 		.then((response) => {
 			//hook.send(typeof response.data);
 			if(response.data.includes('<button class="btn btn-disabled btn-lg btn-block add-to-cart-button"')) {
@@ -63,7 +72,13 @@ const listenLoop = () => {
 				console.log(`Request #${iterator}. No end in sight`);
 			} else {
 				console.log(`Request #${iterator}. The end is nigh`);
-				hook.send(`${PING_TAG} https://www.bestbuy.com/site/nvidia-geforce-rtx-3090-24gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429434.p?skuId=6429434 GO BUY NOW!!`);
+				hook.send(`${PING_TAG} PRODUCT_URL IN STOCK! GO BUY NOW!!`);
+				if(!purchased && AUTO_BUY) {
+					purchased = true;
+					const dom = new JSDOM(response.data);
+					const addButton = dom.window.document.getElementsByClassName("add-to-cart-button")[0];
+					addButton.click()
+				}
 			}
 			
 
